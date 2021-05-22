60b285c494eee12176c47ef63059800db590ba3a
Mahmoud Ismail <m.ismail@speero.net>
2021-05-22 21:30:36 +0200

    Init HTML structure with static data fetch

--- src/app/weather-forecast-view/weather-forecast-view.component.ts
+++ src/app/weather-forecast-view/weather-forecast-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IWeatherData } from '../data-models/weather-data-model';
 import { WeatherService } from '../services/weather.service'
 
 @Component({
@@ -8,19 +9,24 @@ import { WeatherService } from '../services/weather.service'
 })
 export class WeatherForecastViewComponent implements OnInit {
 
+  dataMode: string = 'daily';
+  weatherData: IWeatherData;
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    navigator.geolocation.getCurrentPosition(({ coords }) => {
-      const long = coords.longitude;
-      const lat = coords.latitude;
+    // navigator.geolocation.getCurrentPosition(({ coords }) => {
+    //   const long = coords.longitude;
+    //   const lat = coords.latitude;
 
-      // console.log(this.request_URL);
-      this.weatherService.fetchWeatherData(long, lat).subscribe(res => {
-        console.log(res);
-      })
-    })
-      
+    //   // console.log(this.request_URL);
+    //   this.weatherService.fetchWeatherData(long, lat).subscribe(res => {
+    //     console.log(res);
+    //   })
+    // })
+    this.weatherData = this.fetchStaticData();
   }
 
+  fetchStaticData(): IWeatherData {
+    return this.weatherService.fetchData()
+  }
 }
