62da3d1fa6f9dad3eae14a615640ab075ebcc9b9
Mahmoud Ismail <m.ismail@speero.net>
2021-05-22 21:59:49 +0200

    init daily-data component

---
+++ src/app/daily-data/daily-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-data',
+  templateUrl: './daily-data.component.html',
+  styleUrls: ['./daily-data.component.css']
+})
+export class DailyDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
