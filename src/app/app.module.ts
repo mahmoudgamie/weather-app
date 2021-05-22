a400b248357b72ec70518950f40ed2ff752ba372
Mahmoud Ismail <m.ismail@speero.net>
2021-05-22 13:34:55 +0200

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { WeatherForecastViewComponent } from './weather-forecast-view/weather-forecast-view.component';
@@ -10,7 +11,8 @@ import { WeatherForecastViewComponent } from './weather-forecast-view/weather-fo
     WeatherForecastViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
