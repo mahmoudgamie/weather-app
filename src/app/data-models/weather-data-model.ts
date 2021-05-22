330a97a1f23e319f146b7211ae6197e5fd338ac1
Mahmoud Ismail <m.ismail@speero.net>
2021-05-22 15:09:45 +0200

    Create interface IWeatherData

---
+++ src/app/data-models/weather-data-model.ts
@@ -0,0 +1,104 @@
+export interface IWeatherData {
+  latitude: number;
+  longitude: number;
+  timezone: string;
+  currently: ICurrentlyData;
+  hourly: {
+    summary: string;
+    icon: string;
+    data: IHourlyData[]
+  },
+  daily: {
+    summary: string;
+    icon: string;
+    data: IDailyData[]
+  },
+  flags: {
+    sources: string[];
+    nearest_station: number;
+    units: string
+  },
+  offset: number
+}
+
+interface ICurrentlyData {
+  time: number;
+  summary: string;
+  icon: string;
+  precipIntensity: number;
+  precipProbability: number;
+  temperature: number;
+  apparentTemperature: number;
+  dewPoint: number;
+  humidity: number;
+  pressure: number;
+  windSpeed: number;
+  windGust: number;
+  windBearing: number;
+  cloudCover: number;
+  uvIndex: number;
+  visibility: number;
+  ozone: number;
+}
+
+interface IHourlyData {
+  time: number;
+  summary: string;
+  icon: string;
+  precipIntensity: number;
+  precipProbability: number;
+  temperature: number;
+  apparentTemperature: number;
+  dewPoint: number;
+  humidity: number;
+  pressure: number;
+  windSpeed: number;
+  windGust: number;
+  windBearing: number;
+  cloudCover: number;
+  uvIndex: number;
+  visibility: number;
+  ozone: number;
+}
+
+
+interface IDailyData {
+  time: number;
+  summary: string;
+  icon: string;
+  sunriseTime: number;
+  sunsetTime: number;
+  moonPhase: number;
+  precipIntensity: number;
+  precipIntensityMax: number;
+  precipIntensityMaxTime: number;
+  precipProbability: number;
+  temperatureHigh: number;
+  temperatureHighTime: number;
+  temperatureLow: number;
+  temperatureLowTime: number;
+  apparentTemperatureHigh: number;
+  apparentTemperatureHighTime: number;
+  apparentTemperatureLow: number;
+  apparentTemperatureLowTime: number;
+  dewPoint: number;
+  humidity: number;
+  pressure: number;
+  windSpeed: number;
+  windGust: number;
+  windGustTime: number;
+  windBearing: number;
+  cloudCover: number;
+  uvIndex: number;
+  uvIndexTime: number;
+  visibility: number;
+  ozone: number;
+  temperatureMin: number;
+  temperatureMinTime: number;
+  temperatureMax: number;
+  temperatureMaxTime: number;
+  apparentTemperatureMin: number;
+  apparentTemperatureMinTime: number;
+  apparentTemperatureMax: number;
+  apparentTemperatureMaxTime: number;
+}
