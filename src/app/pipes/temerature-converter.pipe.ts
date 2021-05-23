a61be74ea92a64e5537bcf66ee5f7d3075f38125
Mahmoud Ismail <m.ismail@speero.net>
2021-05-23 15:31:33 +0200

    Cahnge variable dec from var to const

--- src/app/pipes/temerature-converter.pipe.ts
+++ src/app/pipes/temerature-converter.pipe.ts
@@ -13,7 +13,7 @@ export class TemeratureConverterPipe implements PipeTransform {
   transform(value: number, unit: string): unknown {
     if (value && !isNaN(value)) {
       if (unit === 'C') {
-        var tempareature = (value - 32) / 1.8;
+        const tempareature = (value - 32) / 1.8;
         return tempareature.toFixed(2);
       }
       if (unit === 'F') {
